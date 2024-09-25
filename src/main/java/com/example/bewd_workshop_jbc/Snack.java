bab82fc591dd3b49dabccee8584fe1d9799530c0
Jae Dreijling <j.dreijling@student.han.nl>
2024-09-25 13:49:56 +0200

    added snackrow

--- src/main/java/com/example/bewd_workshop_jbc/Snack.java
+++ src/main/java/com/example/bewd_workshop_jbc/Snack.java
@@ -15,4 +15,9 @@ public class Snack {
                 ", calorieen=" + calorieen +
                 '}';
     }
+
+
+
 }
+
+
--- src/main/java/controller/myController.java
+++ src/main/java/controller/myController.java
@@ -1,9 +1,11 @@
 package controller;
 
+import com.example.bewd_workshop_jbc.SnackResultSetExtractor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class myController {
@@ -24,5 +26,10 @@ public class myController {
     public String testData() {
         return jdbcTemplate.queryForObject("SELECT snacknaam FROM Snack WHERE snacknr = 1", String.class);
     }
-}
+
+    @GetMapping("/testdatatwo")
+    public String testDataTwo(){
+        return jdbcTemplate.query("SELECT * FROM Snack", new SnackResultSetExtractor()).toString();
+
+    }}
 
